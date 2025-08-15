b75dbae6c9b17b9914c68c0cb68e6969f35a28df
CVS CHARAN <charan.cvs@gmail.com>
2025-08-15 10:36:34 +0530

    gAuth logic change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+
 
 import Layout from "./components/layout/Layout";
 import Landing from "./pages/Landing";
